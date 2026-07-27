211a7cec68a127205d1464ae6520a7a8a9571644
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-07-27 22:29:49 +0300

    feat: add application error utility

---
+++ src/utils/AppError.ts
@@ -0,0 +1,38 @@
+import { StatusCodes } from "http-status-codes";
+
+export class AppError extends Error {
+    public readonly statusCode: number;
+    public readonly isOperational: boolean;
+
+    constructor(message: string,statusCode: number = StatusCodes.INTERNAL_SERVER_ERROR) {
+        super(message);
+
+        this.statusCode = statusCode;
+        this.isOperational = true;
+
+        Error.captureStackTrace(this, this.constructor);
+    }
+
+
+    static badRequest(message: string) {
+        return new AppError(
+            message,
+            StatusCodes.BAD_REQUEST
+        );
+    }
+
+    static notFound(message = "Resource not found") {
+        return new AppError(
+            message,
+            StatusCodes.NOT_FOUND
+        );
+    }
+
+
+    static conflict(message: string) {
+        return new AppError(
+            message,
+            StatusCodes.CONFLICT
+        );
+    }
+}
