47cf1317a188dbb897e9b0829b36fc6ad7fe1dd7
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-07-29 12:51:42 +0300

    refactor(users): update user controller handlers

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,11 +1,12 @@
 import type { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
 import { UserService } from "./user.service.js";
+import type { CreateUserInput, UpdateUserInput} from "./user.dto.js";
 
 const userService = new UserService();
 
 export async function createUser(
-    req: Request,
+    req: Request<{}, {}, CreateUserInput>,
     res: Response
 ): Promise<void> {
 
@@ -14,6 +15,65 @@ export async function createUser(
     res.success({
         code: StatusCodes.CREATED,
         message: "User created successfully",
+        data: user
+    });
+}
+
+export async function getUsers(
+    _req: Request,
+    res: Response
+): Promise<void> {
+
+    const users = await userService.getUsers();
+
+    res.success({
+        code: StatusCodes.OK,
+        message: "Users fetched successfully",
+        data: users
+    });
+}
+
+export async function getUserById(
+    req: Request<{ id: string }>,
+    res: Response
+): Promise<void> {
+
+    const user = await userService.getUserById(req.params.id);
+
+    res.success({
+        code: StatusCodes.OK,
+        message: "User fetched successfully",
+        data: user
+    });
+}
+
+export async function updateUser(
+    req: Request<{ id: string }, {}, UpdateUserInput>,
+    res: Response
+): Promise<void> {
+
+    const user = await userService.updateUser(
+        req.params.id,
+        req.body
+    );
+
+    res.success({
+        code: StatusCodes.OK,
+        message: "User updated successfully",
+        data: user
+    });
+}
+
+export async function deleteUser(
+    req: Request<{ id: string }>,
+    res: Response
+): Promise<void> {
+
+    const user = await userService.deleteUser(req.params.id);
+
+    res.success({
+        code: StatusCodes.OK,
+        message: "User deleted successfully",
         data: user
     });
 }
