100a8acf7097ef07a57a21e2ca982af38a6d9889
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-07-31 23:32:18 +0300

    chore(config): update dependencies and environment settings

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,10 +2,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log(process.env.MONGODB_URI);
-
 export const env = {
     MONGODB_URI: process.env.MONGODB_URI as string,
+
     PORT: Number(process.env.PORT ?? 3000),
-    NODE_ENV: process.env.NODE_ENV ?? "development"
+
+    NODE_ENV: process.env.NODE_ENV ?? "development",
+
+    ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+
+    REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
+
+    ACCESS_TOKEN_EXPIRES: process.env.ACCESS_TOKEN_EXPIRES ?? "15m",
+
+    REFRESH_TOKEN_EXPIRES: process.env.REFRESH_TOKEN_EXPIRES ?? "30d"
 };
