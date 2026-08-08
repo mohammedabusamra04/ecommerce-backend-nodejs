3b6704c2e981198d6d41c9ec26fc4607f79b4654
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-08-08 21:05:55 +0300

    feat(routes): add category routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { authRoutes } from "../modules/auth/auth.routes.js";
 import { userRoutes } from "../modules/users/user.routes.js";
-
+import { categoryRoutes } from "../modules/products/category/category.routes.js";
 const router = Router();
 
 router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
+router.use("/categories", categoryRoutes);
 
 export default router;
