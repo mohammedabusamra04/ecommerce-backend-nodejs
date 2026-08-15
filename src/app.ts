a1f4f0b53bcef6db4052d3a9fa4539ac498df369
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-08-15 19:10:17 +0300

    refactor(routes): update module route structure

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,9 @@ import { errorHandler } from "./middlewares/error.middleware.js";
 
 const app = express();
 
-app.use(express.json());
-
 app.use(responseFormatter);
+
+app.use(express.json());
 
 app.use("/api", routes);
 
