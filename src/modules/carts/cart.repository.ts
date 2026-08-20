3c899b0ec946d4d1218b75bddd9a68e7dea5fef3
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-08-20 14:35:59 +0300

    feat(carts): implement add or update operation

--- src/modules/carts/cart.repository.ts
+++ src/modules/carts/cart.repository.ts
@@ -1,6 +1,6 @@
 import { Cart } from "./cart.model.js";
 
-interface CartItemData {
+interface AddOrUpdateCartData {
   variantId: string;
   quantity: number;
 }
@@ -8,12 +8,27 @@ interface CartItemData {
 export class CartRepository {
   constructor(private cartModel: typeof Cart) {}
 
-  async findByUser(userId: string) {
-    return this.cartModel
-      .findOne({
-        user: userId,
-      })
-      .populate([
+  async addOrUpdate(
+    userId: string,
+    data: AddOrUpdateCartData
+  ) {
+    const updatedCart = await this.cartModel.findOneAndUpdate(
+        {
+          user: userId,
+          "items.variant": data.variantId,
+        },
+        {
+          $inc: {
+            "items.$.quantity": data.quantity,
+          },
+        },
+        {
+          new: true,
+        }
+      );
+
+    if (updatedCart) {
+      return updatedCart.populate([
         {
           path: "user",
           select: "name -_id",
@@ -23,88 +38,25 @@ export class CartRepository {
           select: "sku price attributes -_id",
         },
       ]);
-  }
+    }
 
-  async create(
-    userId: string,
-    data: CartItemData
-  ) {
-    const cart = await this.cartModel.create({
-      user: userId,
-      items: [
+    const cart =
+      await this.cartModel.findOneAndUpdate(
         {
-          variant: data.variantId,
-          quantity: data.quantity,
+          user: userId,
         },
-      ],
-    });
-
-    return cart.populate([
-      {
-        path: "user",
-        select: "name -_id",
-      },
-      {
-        path: "items.variant",
-        select: "sku price attributes -_id",
-      },
-    ]);
-  }
-
-  async addItem(
-    userId: string,
-    data: CartItemData
-  ) {
-    const cart = await this.cartModel.findOneAndUpdate(
-      {
-        user: userId,
-        "items.variant": {
-          $ne: data.variantId,
-        },
-      },
-      {
-        $push: {
-          items: {
-            variant: data.variantId,
-            quantity: data.quantity,
+        {
+          $push: {
+            items: {
+              variant: data.variantId,
+              quantity: data.quantity,
+            },
           },
         },
-      },
-      {
-        new: true,
-      }
-    );
-
-    return cart?.populate([
-      {
-        path: "user",
-        select: "name -_id",
-      },
-      {
-        path: "items.variant",
-        select: "sku price attributes -_id",
-      },
-    ]);
-  }
-
-  async updateItem(
-    userId: string,
-    data: CartItemData
-  ) {
-    const cart = await this.cartModel.findOneAndUpdate(
-      {
-        user: userId,
-        "items.variant": data.variantId,
-      },
-      {
-        $inc: {
-          "items.$.quantity": data.quantity,
-        },
-      },
-      {
-        new: true,
-      }
-    );
+        {
+          new: true,
+        }
+      );
 
     return cart?.populate([
       {
