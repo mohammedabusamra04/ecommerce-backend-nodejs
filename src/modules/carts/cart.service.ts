b3cb164ce97cfe04b93a133cf60769163daef373
mohammedabusamra04 <mohammedabosamra22@gmail.com>
2026-08-20 14:36:00 +0300

    refactor(carts): simplify cart service logic

--- src/modules/carts/cart.service.ts
+++ src/modules/carts/cart.service.ts
@@ -1,47 +1,19 @@
-import type { AddOrUpdateCartDTO } from "./cart.dto.js";
 import type { CartRepository } from "./cart.repository.js";
-import type { VariantRepository } from "../variants/variant.repository.js";
-import { AppError } from "../../utils/AppError.js";
 
 export class CartService {
-  constructor(
-    private cartRepository: CartRepository,
-    private variantRepository: VariantRepository
-  ) {}
+  constructor( private cartRepository: CartRepository ) {}
 
   async addOrUpdateCart(
     userId: string,
-    data: AddOrUpdateCartDTO
+    variantId: string,
+    quantity: number
   ) {
-    const variant = await this.variantRepository.findBySkuRaw(data.variantSku);
-
-    if (!variant) {
-      throw AppError.notFound("Variant not found");
-    }
-
-    const cart = await this.cartRepository.findByUser(userId);
-
-    const cartItemData = {
-      variantId: variant._id.toString(),
-      quantity: data.quantity,
-    };
-
-    
-    if (!cart) {
-      return this.cartRepository.create(userId,cartItemData);
-    }
-
-    const itemExists = cart.items.some(
-      (item) =>
-        item.variant.toString() ===
-        cartItemData.variantId
+    return this.cartRepository.addOrUpdate(
+      userId,
+      {
+        variantId,
+        quantity,
+      }
     );
-
-    
-    if (itemExists) {
-      return this.cartRepository.updateItem(userId,cartItemData);
-    }
-
-    return this.cartRepository.addItem(userId,cartItemData);
   }
 }
